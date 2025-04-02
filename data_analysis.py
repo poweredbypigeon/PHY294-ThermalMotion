7561a2acd6da863e6548f574b81eb22021eefe5d
This-Username-Is-Missing <75860945+poweredbypigeon@users.noreply.github.com>
2025-04-02 12:37:53 -0400

    added histogram

--- data_analysis.py
+++ data_analysis.py
@@ -1,10 +1,31 @@
 import numpy as np 
-
+import matplotlib.pyplot as plt
 import pandas as pd 
 import os 
 import uncertainties 
 import fit_black_box as bb 
 import math
+from scipy.optimize import curve_fit
+
+
+# Constants
+r = uncertainties.ufloat(9.5e-7, 5e-8) # bead radius, divided by 2. 
+eta = uncertainties.ufloat(1.00, 0.05) # viscosity
+T = uncertainties.ufloat(296.5, 0.5) # temperature
+eta_adjusted = eta * math.pow(0.98, T.n - 293.15) # adjusted viscosity
+gamma = 6 * math.pi * eta_adjusted * r # drag coefficient
+
+print(gamma)
+
+resolution = uncertainties.ufloat(0.1204e-6, 0.003e-6) # camera resolution
+# bead = uncertainties.ufloat(2.0e-6, 0.1e-6) # bead size
+step_size = uncertainties.ufloat(0.5, 0.03) # step size
+
+
+LIST_SIZE = 120 # 120 data points
+
+SOURCE  = "C:\\Users\\compl\\Documents\\Engineering Science\\Y2S2\\PHY294H1\\Labs\\L3+4\\good_data"
+
 
 def adsubprop(series1, series2, unc1, unc2):
     if series1 is None:
@@ -30,11 +51,22 @@ def exprop(series, exponent, unc):
     return result, uncertainty
 
 def normalize(df):
+    
     x_series = df.iloc[:, 0]
     x_series -= x_series[0]
     y_series = df.iloc[:, 1]
     y_series -= y_series[0]
     df = pd.DataFrame({"x": x_series, "y": y_series})
+    return df
+
+def conversion(df):
+    x_series = df.iloc[:, 0]
+    x_series = x_series * resolution.n
+    y_series = df.iloc[:, 1]
+    y_series = y_series * resolution.n 
+    x_unc = pd.Series([1e-7] * x_series.size) # uncertainty in position should be a lot bigger than camera uncertainty
+    y_unc = pd.Series([1e-7] * y_series.size)
+    df = pd.DataFrame({"x": x_series, "y": y_series, "x_unc": x_unc, "y_unc": y_unc})
     return df
 
 def get_x2_series(df):
@@ -48,7 +80,7 @@ def linear(x, m, b):
     return m*x + b 
 
 
-def rayleigh(r, D, t):
+def rayleigh(t, r, D):
     return (r/(2*D*t))*np.exp(-(r**2)/(4*D*t))
 
 def maximum_likelihood(r):
@@ -62,15 +94,6 @@ def get_distances(df):
         distances.append(math.sqrt((x_series[i] - x_series[i-1])**2 + (y_series[i] - y_series[i-1])**2))
     return distances
 
-# Constants
-r = uncertainties.ufloat(9.5e-7, 5e-8) # bead radius, divided by 2. 
-eta = uncertainties.ufloat(1.00, 0.05) # viscosity
-T = uncertainties.ufloat(296.5, 0.5) # temperature
-resolution = uncertainties.ufloat(0.1204e-6, 0.003e-6) # camera resolution
-# bead = uncertainties.ufloat(2.0e-6, 0.1e-6) # bead size
-step_size = uncertainties.ufloat(0.5, 0.03) # step size
-
-SOURCE  = "C:\\Users\\compl\\Documents\\Engineering Science\\Y2S2\\PHY294H1\\Labs\\L3+4\\good_data"
 
 filenames = os.listdir(SOURCE)
 # everything in good-data folder
@@ -83,17 +106,21 @@ distances = [] # distances between the two points
 
 s = None 
 s_total = None 
-unc_total = None
+s_unc = None
+
 
 for filename in filenames:
     df = pd.read_csv(os.path.join(SOURCE, filename), delimiter="\t", skiprows=1)
     # print("Number of rows", len(df), "for", filename)
     df = normalize(df)
+    df = conversion(df)
     # calculate x^2 and uncertainty 
-    x2 = (df.iloc[:, 0] * resolution.n) * (df.iloc[:, 0] * resolution.n)
-    x2_uncertainty = 2 * resolution.n * df.iloc[:, 0] * 1e-7 # so much bigger than camera uncertainty ignore it 
+    x2 = df.iloc[:, 0] * df.iloc[:, 0]
+    x2_uncertainty = 2 * abs(df.iloc[:, 0]) * df.iloc[:, 2] # so much bigger than camera uncertainty ignore it
+    # x2 = (df.iloc[:, 0] * resolution.n) * (df.iloc[:, 0] * resolution.n)
+    # x2_uncertainty = 2 * resolution.n * df.iloc[:, 0] * 1e-7 # so much bigger than camera uncertainty ignore it 
 
-    s_total, unc_total = adsubprop(s_total, x2, unc_total, x2_uncertainty)
+    s_total, s_unc = adsubprop(s_total, x2, s_unc, x2_uncertainty)
 
     # s = get_x2_series(df) / len(filenames)
     distances.extend(get_distances(df)) # this is just - add the distances to the list
@@ -103,14 +130,17 @@ for filename in filenames:
     else:
         s_total += s'
     '''
+LIST_SIZE = len(s_total)
 
 s_total = s_total / len(filenames)
-unc_total = unc_total / len(filenames)
+s_unc = s_unc / len(filenames)
 
-print(s_total)
-print(unc_total)
+s_unc[s_unc == 0] = 1e-14 
+
+# print(s_total)
+# print(s_unc)
 # print(distances)
-
+""" 
 
 guesses = [(4e-13, 0)]
 
@@ -126,8 +156,11 @@ titles = ["X-Squared vs, Time for Beads"]
 labels = [("Time (s)", "X-Squared (m^2)")]
 functions = [linear]
 
-y = [i * step_size.n for i in range(0, 120)]
-yerr = [step_size.s] * 120
+time = np.array([i * step_size.n for i in range(0, LIST_SIZE)])
+time_unc = np.array([step_size.s] * LIST_SIZE)
+
+# print(time)
+# print(time_unc)
 
 i = 0
 init_guess = guesses[i]
@@ -136,11 +169,30 @@ xlabel = labels[i][0]
 ylabel = labels[i][1]
 title = titles[i]
 
-popt = bb.plot_fit(functions[i], s_total, y, unc_total, yerr, init_guess=init_guess, font_size=font_size,
+popt = bb.plot_fit(functions[i], time, s_total, time_unc, s_unc, init_guess=init_guess, font_size=font_size,
             xlabel=xlabel, ylabel=ylabel, title=title, filename=filename.replace(".txt", ".pdf"))
+ """
+'''
+1.0207769625639054e-12 +/- 1.2772905432650444e-14
+-8.08275213086696e-13 +/- 1.4848420162119995e-13
+R-squared: 0.9627819273532291
+'''
 
+counts, bin_edges = np.histogram(distances, bins=100, density=True)  # `density=True` normalizes the histogram
+bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2  # Compute bin centers
+print(bin_centers)
 
-
+# Fit the Gaussian model to the histogram data
+popt, pcov = curve_fit(rayleigh, bin_centers, counts, p0=[5e-6, 2e-23])  # Initial guesses for [a, mu, sigma]
+print(f"Fitted parameters")
+print(popt)
+'''
+plt.hist(distances, bins=100, color='blue', edgecolor='black')  # Set bins to 20
+plt.xlabel('Value')
+plt.ylabel('Frequency')
+plt.title('Histogram Example')
+plt.show()
+'''
 
 """ 
 def pre_process(df):
